1615fd124074168adc5212183e4cde83a60d485d
Arsalan <sharpkhan892@gmail.com>
2025-09-16 21:55:54 +0500

    Day 11 - Javascript basics (variables, types, operations)

---
+++ Day11-js-basics/script.js
@@ -0,0 +1,32 @@
+let name = "ARSALAN";
+let age = 28;
+let isdev = true;
+
+console.log("Hello "+ name + "!") //string
+console.log("Age: " + age)  //variable
+console.log("Are you a dev? ", isdev)   //boolean
+
+//numbers
+let x = 10;
+let y = 3;
+
+//math
+console.log("Addition: ", x + y);
+console.log("Subtract: ", x - y);
+console.log("Multiplication: ", x * y);
+
+//strings
+
+let first = "Python";
+let second = "Webdev";
+console.log("concat: " + first + " " + second)
+
+//modern way
+
+console.log(` i am learning ${first} and ${second}`)
+
+//boolean
+let a = 5;
+let b = 9;
+console.log("is a < b? ", a < b);
+console.log("is a ==b?", a == b);
